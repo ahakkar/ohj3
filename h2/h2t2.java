c4ef70d374e94a4c60a990370428c6750c1a2942
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-01-09 22:03:06 +0200

    java hello world test commit

---
+++ h2/h2t2.java
@@ -0,0 +1,9 @@
+public class h2t2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.print("Hello world!\n");
+    }
+}
