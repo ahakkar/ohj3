25b02d66b4fca7c68797ba160d87fb0abc43415b
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-02-16 15:01:04 +0200

    r6 h1

---
+++ round6/shapes/IShapeMetrics.java
@@ -0,0 +1,5 @@
+public interface IShapeMetrics {
+    String name = "Shape"; // public static final
+    public double area();
+    public double circumference();
+}
---
+++ round6/shapes/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public String toString() {
+        return String.format(
+            "Rectangle with height %.2f and width %.2f",
+            length, width
+            );
+    }
+
+    public String name() {
+        return "rectangle";
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double circumference() {
+        return 2 * (length + width);
+    }
+
+    /*
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5, 10);
+        System.out.println("Area: " + r.area());
+        System.out.println("Circumference: " + r.circumference());
+    }
+    */
+}
