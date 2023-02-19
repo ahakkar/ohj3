b3289fa95e266a69557c041d4a1104ce0b3a1e76
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-02-19 14:41:40 +0200

    json switch iterator to inner class

--- round7/json/ObjectNode.java
+++ round7/json/ObjectNode.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ObjectNode extends Node implements Iterable<String> {
+
+    public class ObjectNodeIterator implements Iterator<String> {
+        private Iterator<String> iterator;
+    
+    
+        public ObjectNodeIterator(Map<String, Node> map) {
+            iterator = sortSet(map.keySet()).iterator();
+        }
+    
+        public static Set<String> sortSet(Set<String> set) {
+            List<String> list = new ArrayList<>(set);
+            Collections.sort(list);
+            return new LinkedHashSet<>(list);
+        }
+    
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+    
+        @Override
+        public String next() {
+            return iterator.next();
+        }
+    
+        @Override
+        public void remove() {
+            iterator.remove();
+        }
+    }
 
     private HashMap<String, Node> map;
 
