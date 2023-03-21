017b132de10e3efcce65e84ccb224dee420fa2a2
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-03-21 13:50:52 +0200

    remove media from module-info

--- round12/calc/src/main/java/module-info.java
+++ round12/calc/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module fi.tuni.prog3.calc {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
-    requires javafx.media;
     opens fi.tuni.prog3.calc to javafx.fxml;
     exports fi.tuni.prog3.calc;
 }
